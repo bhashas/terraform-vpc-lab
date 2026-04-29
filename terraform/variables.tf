1720ed9432fd41f12382a609f3e56f1e94da83b6
bhashas <b.hashas@laposte.net>
2026-04-29 09:24:50 +0200

    update terraform infra configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,45 +1,35 @@
-#############################
-# VARIABLES GLOBALES       #
-#############################
-
-# Région AWS utilisée par le provider
 variable "aws_region" {
-  description = "La région AWS cible"
+  description = "AWS region"
   type        = string
-  default     = "eu-west-3" # Paris (symbolique, même si on ne déploie pas)
+  default     = "eu-west-3"
 }
 
-# CIDR du VPC principal
 variable "vpc_cidr" {
-  description = "CIDR du VPC principal"
+  description = "Main VPC CIDR"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-# CIDR du subnet public
 variable "public_subnet_cidr" {
-  description = "CIDR du subnet public"
+  description = "Public subnet CIDR"
   type        = string
   default     = "10.0.1.0/24"
 }
 
-# CIDR du subnet privé
 variable "private_subnet_cidr" {
-  description = "CIDR du subnet privé"
+  description = "Private subnet CIDR"
   type        = string
   default     = "10.0.2.0/24"
 }
 
-# Nom du projet (pour les tags)
 variable "project_name" {
-  description = "Nom du projet / application"
+  description = "Project name"
   type        = string
   default     = "terraform-vpc-lab"
 }
 
-# Environnement (dev, staging, prod)
 variable "environment" {
-  description = "Environnement de déploiement"
+  description = "Environment name"
   type        = string
   default     = "dev"
 }
